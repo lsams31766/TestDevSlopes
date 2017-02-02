ebe19305d4c8e0d5abce7c26741c4cf0841190db
Lawrence Samuels <lawrencesamuels@lawrences-imac.home>
2017-02-01 19:04:17 -0500

    Initial Commit

---
+++ TestDevSlopes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestDevSlopes
+//
+//  Created by Lawrence Samuels on 2/1/17.
+//  Copyright © 2017 Lawrence Samuels. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
